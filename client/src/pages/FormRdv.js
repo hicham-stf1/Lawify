f8494a427814d0ce60cded02600159abe5a3f5ff
hicham <hzaydi24@gmail.com>
2022-04-10 21:16:28 +0000

    r

--- client/src/pages/FormRdv.js
+++ client/src/pages/FormRdv.js
@@ -45,7 +45,7 @@ function FormRdv() {
       "/api/calendar/" + appointmentId + "/" + user._id,
       values
     );
-    navigate("/");
+    window.location.href = "/appointments";
   };
 
   useEffect(() => {
@@ -115,7 +115,11 @@ function FormRdv() {
               handleChange={handleChange}
             />
           </div>
-          <button type="submit" className="btn btn-block">
+          <button
+            type="submit"
+            onClick={() => (window.location.href = "/appointements")}
+            className="btn btn-block"
+          >
             Confirmer
           </button>
         </form>
